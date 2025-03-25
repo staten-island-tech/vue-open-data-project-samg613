684321eef2586a1100b422a9338a496d5f619678
samg613 <samuelg380@nycstudents.net>
2025-03-24 22:03:29 -0400

    home

--- app/src/components/SearchForm.vue
+++ app/src/components/SearchForm.vue
@@ -1,44 +1,19 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-})
-</script>
-
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <input v-model="query" placeholder="Search by violation..." @input="emitSearch" />
 </template>
 
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
+<script>
+import { ref } from 'vue';
 
-h3 {
-  font-size: 1.2rem;
-}
+export default {
+  setup(_, { emit }) {
+    const query = ref('');
 
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
+    const emitSearch = () => {
+      emit('search', query.value);
+    };
 
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
+    return { query, emitSearch };
+  },
+};
+</script>
